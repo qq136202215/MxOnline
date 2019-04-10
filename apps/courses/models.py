b04c0c9afdc13d8600ed433f07bc5f6d56f26ac5
CZH <136202215@qq.com>
2019-04-10 09:55:45 +0800

    各数据表的制作及主题，界面全局配置

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -9,7 +9,7 @@ class Course(models.Model):
     name = models.CharField(max_length=50, verbose_name=u"课程名")
     desc = models.CharField(max_length=300, verbose_name=u"课程描述")
     detail = models.TextField(verbose_name=u'课程详情')
-    degree = models.CharField(choices=(("cj", "初级"), ("zj", "中级"),("gj", "高级")), max_length=2)
+    degree = models.CharField(verbose_name=u'难度', choices=(("cj", "初级"), ("zj", "中级"),("gj", "高级")), max_length=2)
     learn_times = models.IntegerField(default=0, verbose_name=u"学习时长(分钟数)")
     students = models.IntegerField(default=0, verbose_name=u"学习人数")
     fav_nums = models.IntegerField(default=0, verbose_name=u"收藏人数")
@@ -21,6 +21,9 @@ class Course(models.Model):
         verbose_name = u"课程"
         verbose_name_plural = verbose_name
 
+    def __str__(self):
+        return self.name
+
 
 class Lesson(models.Model):
     course = models.ForeignKey(Course, verbose_name=u"课程", on_delete=models.CASCADE)
@@ -31,15 +34,21 @@ class Lesson(models.Model):
         verbose_name = u"章节"
         verbose_name_plural = verbose_name
 
+    def __str__(self):
+        return self.name
+
 
 class Video(models.Model):
-    lesson = models.ForeignKey(Lesson,verbose_name=u"章节", on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, verbose_name=u"章节", on_delete=models.CASCADE)
     name = models.CharField(max_length=100, verbose_name=u"视频名")
     add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
 
     class Meta:
         verbose_name = u"视频"
         verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
 
 
 class CourseResource(models.Model):
@@ -51,3 +60,6 @@ class CourseResource(models.Model):
     class Meta:
         verbose_name = u"课程资源"
         verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
